523e535432fca0c052d341f37f66a4987e5c77d6
Grzegorz Dzieniszewski <01158044@pw.edu.pl>
2021-06-02 15:30:54 +0000

    Pierwsza implementacja kasy

---
+++ Kasa.h
@@ -0,0 +1,29 @@
+#ifndef CLASS_KASA_H
+#define CLASS_KASA_H
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Kasa {
+  protected:
+    int num;
+    int money;
+    bool isopen;
+    std::list<Klient> queue;
+  public:
+    Kasa(int, unsigned long int, bool);
+    ~Kasa();
+    int getnum();
+    unsigned long int getmoney();
+    bool isKasaopened();
+    void setnum(int);
+    void setmoney(unsigned long int);
+    void openKasa();
+    void closeKasa();
+    void addtoQueue(Klient);
+    void removefromQueue();
+    
+};
+
+#endif
