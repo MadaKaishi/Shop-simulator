1e8afefd60057446fa4e6a1f845ccc4211b3c230
Grzegorz Dzieniszewski <01158044@pw.edu.pl>
2021-06-02 19:09:13 +0000

    Upload New File

---
+++ Kasa.h
@@ -0,0 +1,34 @@
+#ifndef KASA_H
+#define KASA_H
+
+#include <iostream>
+#include <list>
+//#include "Klient.h"
+using namespace std;
+
+class Kasa
+{
+public:
+  int num, money, tury_pracy, tury_przerwy;
+  bool isopen;
+  //std::list<Klient> queue;
+
+  Kasa();
+  Kasa(int n, int cash, bool o);
+  ~Kasa();
+  int getnum();
+  int getmoney();
+  bool isKasaopened();
+  void setnum(int);
+  void setmoney(int);
+  void openKasa();
+  void closeKasa();
+  //void addtoQueue(Klient);
+  void removefromQueue();
+  void increment_tury_pracy();
+  void clear_tury_pracy();
+  void increment_tury_przerwy();
+  void clear_tury_przerwy();
+};
+
+#endif
